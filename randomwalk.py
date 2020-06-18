3aaca476c03a3fe7776dc8a338a64b935a550014
FredHobel <31831984+FredHobel@users.noreply.github.com>
2020-06-18 14:37:14 -0700

    Update randomwalk.py

--- randomwalk.py
+++ randomwalk.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rn
+import tkinter as t
 
 
 j = 0
@@ -66,6 +67,9 @@ for l in range(loop):
         averagex[n] += x1[n]
         averagey[n] += y1[n]
         size[n] += 1
+
+
+
     
     
 plt.plot(boundaryx, boundaryy, '--r')
@@ -84,4 +88,63 @@ plt.plot(averagex, averagey, '-b')
 plt.plot(boundaryx, boundaryy, '--r')
 plt.show()
 
+bol = 0
+boo = 0
+ool = 0
+dev = 0
+for z in range(len(size)):
+    if averagex[z] >= xmin and averagex[z] <= xmax and averagey[z] >= ymin and averagey[z] <= ymax and boo != 1:
+        avp = z
+        boo = 1
 
+    if size[z] <= size[0]*0.5 and bol != 1:
+        avind = z
+        bol = 1
+        print(size[z])
+    if size[z] < size[z-1] and ool != 1:
+        small = z
+        ool = 1
+
+    #i am using the y value as the expected x because it is approximating the y=x line
+    dev += (averagex[z] - averagey[z])**2
+    rmsd = (dev/len(size))**(0.5)
+
+rel = t.Tk()
+
+rel.geometry("600x250")
+
+rel.title("Compare")
+
+L1 = t.Label(rel, text="individual path average length: ")
+L1.pack()
+
+L2 = t.Label(rel, text=avind)
+L2.pack()
+
+L3 = t.Label(rel, text="averaged path total length: ")
+L3.pack()
+
+L4 = t.Label(rel, text= avp)
+L4.pack()
+
+L5 = t.Label(rel, text="shortest path length: ")
+L5.pack()
+
+L6 = t.Label(rel, text=small)
+L6.pack()
+
+L7 = t.Label(rel, text="longest path length: ")
+L7.pack()
+
+L8 = t.Label(rel, text=len(size))
+L8.pack()
+
+L9 = t.Label(rel, text="the RMS deviation from a straight line of averaged path: ")
+L9.pack()
+
+L10 = t.Label(rel, text=rmsd)
+L10.pack()
+
+rel.mainloop()
+
+print(rmsd)
